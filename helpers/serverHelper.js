1c50a8332baf9612b378a2480e7af6dda9936541
Nick Sadler <nicksadler14@gmail.com>
2022-12-30 16:35:09 -0500

    remove puppeteer stuff

--- helpers/serverHelper.js
+++ helpers/serverHelper.js
@@ -7,54 +7,6 @@ const gameCategoriesWeWant = [1215, 1216, 1217, 1218, 583, 1219, 1220]
 const blocksStealsNames = ["Blocks ", "Steals ", "Steals + Blocks"]
 const comboSubcategoryNames = ["Pts + Reb + Ast","Pts + Reb","Pts + Ast","Ast + Reb"]
 
-const searchForETRProjections = async () => {
-  const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-} );
-  const page = await browser.newPage();
-  await page.goto('https://establishtherun.com/wp-login.php?redirect_to=%2F')
-  await page.waitForTimeout(5000)
-  await page.type('input[name=log]', 'nick.sadler')
-  await page.type('input[name=pwd]', 'NickSadler11$')
-  await page.click('input[name=wp-submit]')
-  await page.waitForTimeout(10000)
-  await page.goto('https://establishtherun.com/daily-nba-full-statistical-projections/');
-  await page.waitForTimeout(10000)
-  const data = await page.evaluate(() => {
-    const tds = Array.from(document.querySelectorAll('table tr td'))
-    return tds.map(td => td.innerText.trim())
-  });
-  //console.log(data)
-  let chunkedData = []
-  const chunkSize = 11;
-  for (let i = 0; i < data.length; i += chunkSize) {
-    const chunk = data.slice(i, i + chunkSize);
-    let dataObject = {
-      player: chunk[0],
-      team1: chunk[1],
-      team2: chunk[2],
-      minutes: Number(chunk[3]).toFixed(2),
-      points: Number(chunk[4]).toFixed(2),
-      assists: Number(chunk[5]).toFixed(2),
-      rebounds: Number(chunk[6]).toFixed(2),
-      turnovers: Number(chunk[7]).toFixed(2),
-      threes: Number(chunk[8]).toFixed(2),
-      steals: Number(chunk[9]).toFixed(2),
-      blocks: Number(chunk[10]).toFixed(2),
-      stealsNblocks: Number(chunk[9])+Number(chunk[10]).toFixed(2),
-      PRA: Number(chunk[4])+Number(chunk[5])+Number(chunk[6]).toFixed(2),
-      PR: Number(chunk[4])+Number(chunk[5]).toFixed(2),
-      PA: Number(chunk[4])+Number(chunk[6]).toFixed(2),
-      AR: Number(chunk[5])+Number(chunk[6]).toFixed(2),
-    }
-    chunkedData.push(dataObject)
-  }
-  console.log(chunkedData.length)
-  //await page.screenshot({path: 'screenshot.png', fullPage: true});
-  browser.close();
-  return chunkedData
-}
-
 const searchDKForEvents = async () => {
   const url = 'https://sportsbook-us-nh.draftkings.com//sites/US-NH-SB/api/v5/eventgroups/42648?format=json'
     
@@ -82,7 +34,6 @@ const searchDKEventForProps = async (eventID) => {
 
   return resp
 }
-
 
 module.exports = {
   etrSearch: async function() {
@@ -546,30 +497,7 @@ module.exports = {
     flatArray = [].concat(...propData.PA);
     propData.PA = flatArray
 
-    const chunkedETRData = await searchForETRProjections()
-    // do the finalData combinations
-    let finalData = []
-    for (const playerChunk of chunkedETRData) {
-      let playerChunkData = {}
-      playerChunkData["player"] = playerChunk.player
-      playerChunkData["team1"] = playerChunk.team1
-      playerChunkData["team2"] = playerChunk.team2
-      playerChunkData["minutes"] = playerChunk.minutes
-
-      let playerCategoryData = propData.points.filter((line) => line.player.toUpperCase() === playerChunk.player.toUpperCase())
-      console.log(playerCategoryData)
-      console.log(playerChunk.player)
-      if (playerCategoryData.length > 0) {
-
-      } else {
-        playerChunkData["points"] = {
-          
-        }
-      }
-      
-    }
-
-    await client.set("data", JSON.stringify(propData), {'EX': 3600})
+    await client.set("propdata", JSON.stringify(propData), {'EX': 3600})
     console.log("Done!")
     return propData
   }
--- index.js
+++ index.js
@@ -66,12 +66,7 @@ setInterval(async () => {
   const date = new Date();
   console.log(`Current hour: ${date.getHours()}`)
   if (date.getHours() >= 17 && date.getHours() <= 23) {  
-    const delay = Math.floor(Math.random() * (900000 - 10000 + 1)) + 10000;
-    console.log(`Delaying ${delay/1000/60} Minutes`)
-    await sleep(delay)
-    console.log("Done syncing latest tokens")
-  } else {
-    console.log("Not the right time of day!")
+    serverHelper.search(redisClient)
   }
 }, 1800000);
 
