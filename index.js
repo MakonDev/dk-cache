f321e5c2e6a65956828158cab67f2bd163bca57e
Nick Sadler <nicksadler14@gmail.com>
2022-12-31 00:29:21 -0500

    more logging

--- index.js
+++ index.js
@@ -108,8 +108,10 @@ app.post("/acceptETR", async(req, res) => {
 setInterval(async () => {
   const date = new Date();
   console.log(`Current hour: ${date.getHours()}`)
-  if (date.getHours() >= 0 && date.getHours() <= 4) {  
+  if (date.getHours() >= 0 && date.getHours() <= 5) {  
     serverHelper.search(redisClient)
+  } else {
+    console.log("Out of hours!")
   }
 }, 300000);
 
